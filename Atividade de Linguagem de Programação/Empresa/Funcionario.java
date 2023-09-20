423bee3eec5b35751b80b5208ee09c8ff6ad8195
Thiago Sousa <thiagosousa81@gmail.com>
2023-09-20 15:20:07 -0300

    §

--- Atividade de Linguagem de Programação/Empresa/Funcionario.java
+++ Atividade de Linguagem de Programação/Empresa/Funcionario.java
@@ -1,4 +1,11 @@
 package Empresa;
 class Funcionario {
-    
+
+    public void cadastrar_Cliente(int codigo, String nome, String telefone, String cpf){
+        Cliente c = new Cliente();
+        c.codigo = codigo;
+        c.nome = nome;
+        c.telefone = telefone;
+        c.cpf = cpf;
+    }
 }
