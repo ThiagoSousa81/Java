b71ba0e27f82b3e100958afe25af4cffd5f5b266
Thiago Sousa <thiagosousa81@gmail.com>
2023-09-20 15:11:57 -0300

    §

--- Atividade de Linguagem de Programação/Empresa/App.java
+++ Atividade de Linguagem de Programação/Empresa/App.java
@@ -5,5 +5,6 @@ class Main {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         
+        
     }
 }
--- Atividade de Linguagem de Programação/Empresa/Cliente.java
+++ Atividade de Linguagem de Programação/Empresa/Cliente.java
@@ -1,4 +1,7 @@
 package Empresa;
 class Cliente {
-
+    int codigo = 0;
+    String nome = "";
+    String telefone = ""; 
+    String cpf = "";
 }
