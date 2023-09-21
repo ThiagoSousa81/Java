f11a1459754d5148c459cb1a49264fecab0f753f
Thiago Sousa <thiagosousa81@gmail.com>
2023-09-21 14:11:33 -0300

    §

--- Atividade de Linguagem de Programação/Empresa/App.java
+++ Atividade de Linguagem de Programação/Empresa/App.java
@@ -5,6 +5,8 @@ class Main {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         
+        System.out.println("Digite a ação que deseja realizar:\n\n1 - Consultar Cliente\n2 - Cadastrar Cliente\n3 - Realizar venda");
+
         
     }
 }
