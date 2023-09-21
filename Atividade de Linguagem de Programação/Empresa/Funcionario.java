94d4140a5065475b3a30c9af378a13eb77c4cf8f
Thiago Sousa <thiagosousa81@gmail.com>
2023-09-21 14:08:45 -0300

    §

--- Atividade de Linguagem de Programação/Empresa/Funcionario.java
+++ Atividade de Linguagem de Programação/Empresa/Funcionario.java
@@ -1,6 +1,7 @@
 package Empresa;
 class Funcionario {
     Cliente c = new Cliente();
+    Vendas v = new Vendas();
     
     public void cadastrar_Cliente(int codigo, String nome, String telefone, String cpf){
         c.codigo = codigo;
@@ -9,8 +10,11 @@ class Funcionario {
         c.cpf = cpf;
     }
 
-    public void realizar_Venda(int codigo_Venda, int codigo_Cliente, int codigo_Funcionario, int codigos_Veiculos){
-
+    public void realizar_Venda(int codigo_Venda, int codigo_Cliente, int codigo_Funcionario, int codigos_Veiculos[]){
+        v.codigo_Venda = codigo_Venda;
+        v.codigo_Cliente = codigo_Cliente;
+        v.codigo_Funcionario = v.codigo_Funcionario;
+        v.codigos_Veiculos[] = codigos_Veiculos[];
     }
 
     public String consultar_Cliente(String nome) {
