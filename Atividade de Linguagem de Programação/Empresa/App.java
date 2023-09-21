3bd48f9113b159b6c500cebdbfacc6c0add31075
Thiago Sousa <thiagosousa81@gmail.com>
2023-09-21 14:17:30 -0300

    §

--- Atividade de Linguagem de Programação/Empresa/App.java
+++ Atividade de Linguagem de Programação/Empresa/App.java
@@ -4,9 +4,24 @@ import java.util.Scanner;
 class Main {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        
-        System.out.println("Digite a ação que deseja realizar:\n\n1 - Consultar Cliente\n2 - Cadastrar Cliente\n3 - Realizar venda");
+        Funcionario f = new Funcionario();
+        String cmd;
 
-        
+        while (true) {
+            System.out.println("Digite a ação que deseja realizar:\n\n1 - Consultar Cliente\n2 - Cadastrar Cliente\n3 - Realizar venda");
+            cmd = sc.next();
+
+            if (cmd == 1) {
+                System.out.println("\nCadastro de Clientes.")
+            }
+            else
+                if (cmd == 2) {
+                System.out.println("\nConsulta de Cliente")
+            }
+            else
+                if (cmd == 3) {
+                System.out.println("\nRealizar venda")
+            }
+        }
     }
 }
