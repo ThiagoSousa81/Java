fe0cd1dd0b3638e0a8630ea9bcfc2f87147e5578
Thiago Sousa <thiagosousa81@gmail.com>
2023-10-15 19:02:25 -0300

    §

--- Atividade de Linguagem de Programação/Empresa/Veiculos.java
+++ Atividade de Linguagem de Programação/Empresa/Veiculos.java
@@ -1,5 +1,7 @@
 package Empresa;
 class Veiculos{
-    int id_veiculo, ano_Veiculo;
-    String nome_Veiculo, marca_Veiculo;
+    private int id_veiculo, ano_Veiculo;
+    private String nome_Veiculo, marca_Veiculo;
+
+
 }
