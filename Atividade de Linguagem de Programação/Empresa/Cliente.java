2ff8d35f461b4109eb537a2ceb5d25bc476924c3
Thiago Sousa <thiagosousa81@gmail.com>
2023-10-15 18:41:15 -0300

    §

--- Atividade de Linguagem de Programação/Empresa/Cliente.java
+++ Atividade de Linguagem de Programação/Empresa/Cliente.java
@@ -1,17 +1,13 @@
 package Empresa;
-class Cliente {
-    int codigo = 0;
+class Cliente {    
     String nome = "";
     String telefone = ""; 
     String cpf = "";
 
-    String[] registros = new String[100][4];
 
-    public void salvar_Cliente() {
-        registros[this.codigo][0] = this.codigo;
-        registros[this.codigo][1] = this.nome;
-        registros[this.codigo][2] = this.telefone;
-        registros[this.codigo][3] = this.cpf;
-        this.codigo++;
+    public void salvar_Cliente(nome, telefone, cpf) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.cpf = cpf;
     }
 }
