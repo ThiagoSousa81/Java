8322f3173e9abee829fb0c3c52ff0d1ed77d5790
Thiago Sousa <thiagosousa81@gmail.com>
2023-10-15 21:05:21 -0300

    §

--- Atividade de Linguagem de Programação/Empresa/Veiculos.java
+++ Atividade de Linguagem de Programação/Empresa/Veiculos.java
@@ -2,10 +2,10 @@ package Empresa;
 class Veiculos{
     private int id_veiculo, ano_Veiculo;
     private String nome_Veiculo, marca_Veiculo;
-    private double preco_Veiculo;
+    private double preco_Veiculo, desconto_Veiculo;
 
     public double retornar_Preco(String nome) {
-        return preco_Veiculo;
+        return preco_Veiculo-((preco_Veiculo*desconto_Veiculo)/100);
     }
 
     public String relatorio() {
