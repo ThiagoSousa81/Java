55a703473484d28beede738ae28ba2a5f6862d6f
Thiago Sousa <thiagosousa81@gmail.com>
2023-10-22 22:59:53 -0300

    §

--- Atividade de Linguagem de Programação/Empresa/App.java
+++ Atividade de Linguagem de Programação/Empresa/App.java
@@ -1,8 +1,8 @@
 package Empresa;
 import java.util.Scanner;
 
-class Main {
-    public static void main(String args[]){
+public class Main {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Funcionario f = new Funcionario();
         String cmd;
@@ -11,23 +11,47 @@ class Main {
         System.out.print("Usuário: ");
         String user = sc.nextLine();
 
-        if (user != "Admin") {
+        if (!user.equals("Admin")) {
             System.out.println("Você não tem permissão de acessar o sistema!");
-            Application.Exit();
+            System.exit(0);
         }
 
         System.out.println("Escolha uma das opções abaixo:\n1 - Cadastro de Funcionários\n2 - Cadastro de Veículos\n3 - Gerar relatórios");
-        cmd.nextLine();
+        cmd = sc.nextLine();
 
-        switch (cmd){
-
-            case cmd == "1" :
-                
-            break;
-
+        switch (cmd) {
+            case "1":
+                System.out.println("Digite o nome do funcionário:");
+                String nome = sc.nextLine();
+                System.out.println("Digite o e-mail do funcionário:");
+                String email = sc.nextLine();
+                System.out.println("Digite o usuário do funcionário:");
+                String usuario = sc.nextLine();
+                f.cadastrar_Funcionario(nome, email, usuario);
+                break;
+            case "2":
+                System.out.println("Digite o nome do veículo:");
+                String nomeVeiculo = sc.nextLine();
+                System.out.println("Digite o preço do veículo:");
+                double preco = sc.nextDouble();
+                car.cadastrar_Veiculo(nomeVeiculo, preco);
+                break;
+            case "3":
+                System.out.println("Digite o nome do cliente:");
+                String nomeCliente = sc.nextLine();
+                System.out.println("Digite o nome do veículo:");
+                String nomeVeiculo = sc.nextLine();
+                System.out.println("Digite o preço do veículo:");
+                double preco = sc.nextDouble();
+                System.out.println("Digite o nome do funcionário:");
+                String nomeFuncionario = sc.nextLine();
+                System.out.println("Digite a data da venda:");
+                String dataVenda = sc.nextLine();
+                v.realizar_Venda(0, nomeCliente, nomeVeiculo, preco, nomeFuncionario, dataVenda);
+                break;
+            default:
+                System.out.println("Função não identificada!");
 
         }
-       
-        
     }
 }
